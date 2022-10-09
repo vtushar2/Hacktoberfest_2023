b86573d04f132bec138c8e3cb4b39e8442a2a8a1
“Ravindu <ravindu.first@gmail.com>
2022-10-10 00:34:36 +0530

    added a new python file with a folder

---
+++ python/check alphabet or not.py
@@ -0,0 +1,10 @@
+#Check a character Alphabet or Not
+
+print("Enter a Character: ")
+c = input()
+if c>='a' and c<='z':
+    print("\nIt is an alphabet")
+elif c>='A' and c<='z':
+    print("\nIt is an alphabet")
+else:
+    print("\nIt is not an alphabet!")
